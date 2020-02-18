14638c24d0e27a2d1ef807e01cd2d88ff8bfc7aa
AJBorbzz <ajbitheaddhz@gmail.com>
2020-02-18 21:29:37 +0800

    Installed Redux; Applied Redux for Payloads; Developed Get, Delete and Add Products

--- exam/frontend/src/components/products/Form.js
+++ exam/frontend/src/components/products/Form.js
@@ -1,13 +1,73 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addProduct } from "../../actions/products";
 
 export class Form extends Component {
+  state = {
+    name: "",
+    description: "",
+    quantity: ""
+  };
+
+  static propTypes = {
+    addProduct: PropTypes.func.isRequired
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    const { name, description, quantity } = this.state;
+    const product = { name, description, quantity };
+    this.props.addProduct(product);
+  };
+
   render() {
+    const { name, description, quantity } = this.state;
     return (
-      <div>
-        <h1>Add Product Form</h1>
+      <div className="card card-body mt-4 mb-4">
+        <h2>Add Product</h2>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              onChange={this.onChange}
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            <label>Description</label>
+            <input
+              className="form-control"
+              type="text"
+              name="description"
+              onChange={this.onChange}
+              value={description}
+            />
+          </div>
+          <div className="form-group">
+            <label>Quantity</label>
+            <input
+              className="form-control"
+              type="text"
+              name="quantity"
+              onChange={this.onChange}
+              value={quantity}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
 }
 
-export default Form;
+export default connect(null, { addProduct })(Form);
--- exam/frontend/src/components/products/Products.js
+++ exam/frontend/src/components/products/Products.js
@@ -1,13 +1,63 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getProducts, deleteProduct } from "../../actions/products";
 
 export class Products extends Component {
+  static propTypes = {
+    products: PropTypes.array.isRequired,
+    getProducts: PropTypes.func.isRequired,
+    deleteProduct: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    this.props.getProducts();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Products List</h1>
-      </div>
+      <Fragment>
+        <h2>Products List</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Quantity</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.products.map(product => (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.name}</td>
+                <td>{product.description}</td>
+                <td>{product.quantity}</td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={this.props.deleteProduct.bind(this, product.id)}
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
     );
   }
 }
 
-export default Products;
+// Map the state to props component (this.props.products)
+const mapStateToProps = state => ({
+  // points to products reducer.
+  products: state.products.products
+});
+
+export default connect(mapStateToProps, { getProducts, deleteProduct })(
+  Products
+);
